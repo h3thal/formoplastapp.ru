77fb8a9b835146b7c231df27b88860ba624adbfe
h3thal <h3thal@yandex.ru>
2021-07-14 20:50:59 +0300

    v 0.2

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,3 +5,21 @@ btnMenu.addEventListener('click', function () {
   this.classList.toggle('active');
   menu.classList.toggle('open');
 });
+
+const links = document.getElementsByClassName('links')[0].getElementsByTagName('a');
+console.log(typeof links)
+Object.values(links).forEach( function (el) {
+  el.addEventListener('click', event => {
+    event.preventDefault();
+    let y = document.getElementById(el.getAttribute('data-scroll')).getBoundingClientRect().top + pageYOffset - 100;
+    window.scrollTo(
+      {
+        top: y,
+        behavior: "smooth"
+    });
+    
+  })
+});
+document.addEventListener('scroll', e => {
+  console.log(window.scrollY);
+})
